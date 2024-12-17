e01f8f1e1cde59248f63435dc354474f4b7493e6
hydev777 <hytechwilson@gmail.com>
2024-12-17 16:41:45 -0400

    Added 4 widget test

--- test/side_menu_animation_test.dart
+++ test/side_menu_animation_test.dart
@@ -98,6 +98,46 @@ void main() {
       expect(_indexSelected, 3);
     });
 
+        testWidgets(
+        'SideMenuAnimation:: Press menu, if press item at index 0 page should not change',
+        (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: SideMenuAnimation(
+            appBarBuilder: (showMenu) => AppBar(
+              leading: IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.black),
+                  onPressed: showMenu),
+            ),
+            views: List.generate(
+                4,
+                (index) => Container(
+                    color: Colors.red,
+                    child: Center(child: Text('Page ${index + 1}')))),
+            items: List.generate(
+              4,
+              (index) => Center(
+                child: Text('Item $index'),
+              ),
+            ).map((value) => value).toList(),
+            tapOutsideToDismiss: true,
+            scrimColor: Colors.black45,
+            onItemSelected: (value) {},
+          ),
+        ),
+      ));
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('Item 2'));
+      await tester.pumpAndSettle();
+      expect(find.text('Page 2'), findsOneWidget);
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('Item 0'));
+      await tester.pumpAndSettle();
+      expect(find.text('Page 2'), findsOneWidget);
+    });
+
     testWidgets(
         'SideMenuAnimation:: Press menu, when in page 1, press item 1 and stay in same page',
         (tester) async {
@@ -135,6 +175,109 @@ void main() {
       await tester.tap(find.text('Item 1'));
       await tester.pumpAndSettle();
       expect(find.text('Page 1'), findsOneWidget);
+    });
+
+    testWidgets(
+        'SideMenuAnimation:: Press menu, when pressed outside and tapOutsideToDismiss is false keep menu open',
+        (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: SideMenuAnimation(
+            appBarBuilder: (showMenu) => AppBar(
+              leading: IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.black),
+                  onPressed: showMenu),
+            ),
+            views: List.generate(
+                4,
+                (index) => Container(
+                    color: Colors.red,
+                    child: Center(child: Text('Page ${index + 1}')))),
+            items: List.generate(
+              4,
+              (index) => Center(
+                child: Text('Item $index'),
+              ),
+            ).map((value) => value).toList(),
+            tapOutsideToDismiss: false,
+            scrimColor: Colors.black45,
+            onItemSelected: (value) {},
+          ),
+        ),
+      ));
+
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pumpAndSettle();
+      expect(find.byType(AnimatedContainer), findsNothing);
+      await tester.tapAt(const Offset(400, 100));
+      expect(find.byType(AnimatedContainer), findsNothing);
+    });
+
+    testWidgets(
+        'SideMenuAnimation:: Press menu, when pressed outside and tapOutsideToDismiss is true close menu',
+        (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: SideMenuAnimation(
+            appBarBuilder: (showMenu) => AppBar(
+              leading: IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.black),
+                  onPressed: showMenu),
+            ),
+            views: List.generate(
+                4,
+                (index) => Container(
+                    color: Colors.red,
+                    child: Center(child: Text('Page ${index + 1}')))),
+            items: List.generate(
+              4,
+              (index) => Center(
+                child: Text('Item $index'),
+              ),
+            ).map((value) => value).toList(),
+            tapOutsideToDismiss: true,
+            scrimColor: Colors.black45,
+            onItemSelected: (value) {},
+          ),
+        ),
+      ));
+
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pumpAndSettle();
+      await tester.tapAt(const Offset(400, 100));
+      await tester.pumpAndSettle();
+      expect(find.byType(AnimatedContainer), findsNothing);
+    });
+
+    testWidgets('SideMenuAnimation:: show AppBar', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: SideMenuAnimation(
+            appBarBuilder: (showMenu) => AppBar(
+              leading: IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {},
+              ),
+            ),
+            views: [
+              Container(
+                color: Colors.red,
+                child: const Center(
+                  child: Text('Page 1'),
+                ),
+              )
+            ],
+            items: [
+              const Center(
+                child: Text('Item 1'),
+              )
+            ],
+            onItemSelected: (value) {},
+          ),
+        ),
+      ));
+
+      expect(find.byType(AppBar), findsOne);
     });
   });
 }
